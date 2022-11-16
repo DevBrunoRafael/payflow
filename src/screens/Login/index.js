9135ec150ba21fcb75a65d0e11491e4bb2efc280
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-11-16 15:37:41 -0300

    refactor: settings screens

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -3,105 +3,152 @@ import { Context } from "../../context/authContext";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Text, Button, TextInput } from "react-native-paper";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+   Image,
+   Keyboard,
+   StatusBar,
+   StyleSheet,
+   TouchableOpacity,
+   TouchableWithoutFeedback,
+   View,
+} from "react-native";
 
 const Login = ({ navigation }) => {
-   
    const { state, teste } = useContext(Context);
 
    const [email, setEmail] = useState("");
    const [password, setPassword] = useState("");
- 
+
    const [showPassword, setShowPassword] = useState(true);
- 
+
    return (
-     <SafeAreaView style={styles.container}>
-       <Text style={styles.login}>Login</Text>
-       <TextInput
-         style={styles.textInput}
-         label="E-mail"
-         value={email}
-         onChangeText={(text) => setEmail(text)}
-         left={
-           <TextInput.Icon
-             name="at"
-             size={25}
-             color="black"
-           />
-         }
-       />
-       <TextInput
-         style={styles.textInput}
-         label="Senha"
-         value={password}
-         onChangeText={(text) => setPassword(text)}
-         left={
-           <TextInput.Icon
-             name="lock"
-             size={25}
-             color="black"
-           />
-         }
-         secureTextEntry={showPassword}
-         right={
-           showPassword ? (
-             <TextInput.Icon
-               name="eye"
-               size={25}
-               color="black"
-               onPress={() => setShowPassword(!showPassword)}
-             />
-           ) : (
-             <TextInput.Icon
-               name="eye-off"
-               size={25}
-               color="black"
-               onPress={() => setShowPassword(!showPassword)}
-             />
-           )
-         }
-       />
-       <Button mode="contained" style={styles.loginButton}>
-         Login
-       </Button>
-       <TouchableOpacity
-         onPress={() => navigation.navigate("Register")}
-       >
-         <Text>
-           Não tem uma conta?{" "}
-           <Text style={styles.createAccountText}>
-             Crie uma
-           </Text>
-         </Text>
-       </TouchableOpacity>
-     </SafeAreaView>
+      <TouchableWithoutFeedback
+         onPress={() => Keyboard.dismiss()}
+         touchSoundDisabled
+      >
+         <SafeAreaView style={styles.container}>
+            <StatusBar
+               barStyle="dark-content"
+               hidden={false}
+               backgroundColor="#FF941A"
+               translucent={false}
+               networkActivityIndicatorVisible={true}
+            />
+            {/* <Text style={styles.login}>Login</Text> */}
+            <View style={styles.cardImg}>
+               <Image
+                  style={styles.img}
+                  source={require("payflow-app/assets/image-cnt.png")}
+               />
+            </View>
+            <View style={styles.cardInputs}>
+               <TextInput
+                  style={styles.textInput}
+                  label="E-mail"
+                  value={email}
+                  mode={"outlined"}
+                  activeOutlineColor={"#FF941A"}
+                  onChangeText={text => setEmail(text)}
+                  outlineColor={"#FF941A"}
+                  left={
+                     <TextInput.Icon
+                        icon="account"
+                        size={25}
+                        iconColor="#FF941A"
+                     />
+                  }
+               />
+               <TextInput
+                  style={styles.textInput}
+                  label="Senha"
+                  value={password}
+                  mode={"outlined"}
+                  activeOutlineColor={"#FF941A"}
+                  onChangeText={text => setPassword(text)}
+                  outlineColor={"#FF941A"}
+                  left={
+                     <TextInput.Icon
+                        icon="lock"
+                        size={25}
+                        iconColor="#FF941A"
+                     />
+                  }
+                  secureTextEntry={showPassword}
+                  right={
+                     showPassword ? (
+                        <TextInput.Icon
+                           icon="eye"
+                           size={25}
+                           iconColor="#FF941A"
+                           onPress={() => setShowPassword(!showPassword)}
+                        />
+                     ) : (
+                        <TextInput.Icon
+                           icon="eye-off"
+                           size={25}
+                           iconColor="#FF941A"
+                           onPress={() => setShowPassword(!showPassword)}
+                        />
+                     )
+                  }
+               />
+               <Button mode="contained" style={styles.loginButton}>
+                  Login
+               </Button>
+               <TouchableOpacity
+                  onPress={() => navigation.navigate("Register")}
+               >
+                  <Text>
+                     Não tem uma conta?{" "}
+                     <Text style={styles.createAccountText}>Crie uma</Text>
+                  </Text>
+               </TouchableOpacity>
+            </View>
+         </SafeAreaView>
+      </TouchableWithoutFeedback>
    );
- };
- 
- const styles = StyleSheet.create({
+};
+
+const styles = StyleSheet.create({
    container: {
-     marginTop: "40%",
-     alignSelf: "center",
-     width: "80%",
+      alignSelf: "center",
+      width: "100%",
    },
    login: {
-     fontSize: 40,
-     fontWeight: "bold",
-     marginBottom: 10,
+      fontSize: 40,
+      fontWeight: "bold",
+      marginBottom: 10,
    },
    textInput: {
-     marginBottom: 10,
+      marginBottom: 5,
+      borderRadius: 5,
    },
    loginButton: {
-     padding: 5,
-     margin: 20,
-     width: "50%",
-     alignSelf: "center",
+      paddingVertical: 4,
+      margin: 20,
+      width: "100%",
+      alignSelf: "center",
+      borderRadius: 5,
+      backgroundColor: "#FF941A",
    },
    createAccountText: {
-     fontWeight: "bold",
-     color: "#6200ee",
+      fontWeight: "bold",
+      color: "#FF941A",
    },
- });
+   cardImg: {
+      width: "100%",
+      height: "60%",
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 25,
+   },
+   img: {
+      width: "100%",
+   },
+   cardInputs: {
+      marginTop: -50,
+      paddingHorizontal: 30,
+   },
+});
 
 export default Login;
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,15 +1,155 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import React, { useContext, useState } from "react";
+import { Context } from "../../context/authContext";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const Register = () => {
+import { Text, Button, TextInput } from "react-native-paper";
+import {
+   Keyboard,
+   StatusBar,
+   StyleSheet,
+   TouchableWithoutFeedback,
+   View,
+} from "react-native";
+
+
+const Register = ({ navigation }) => {
+   const [nome, setNome] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [showPassword, setShowPassword] = useState(false);
+
    return (
-      <SafeAreaView>
-         <Text>Register</Text>
-      </SafeAreaView>
+      <TouchableWithoutFeedback
+         onPress={() => Keyboard.dismiss()}
+         touchSoundDisabled
+      >
+         <SafeAreaView style={styles.container}>
+            <StatusBar
+               barStyle="dark-content"
+               hidden={false}
+               backgroundColor="#FFF"
+               translucent={false}
+               networkActivityIndicatorVisible={true}
+            />
+            <View style={styles.textContainer}>
+               <Text style={styles.register}>Cadastro</Text>
+            </View>
+            <View style={styles.cardInputs}>
+               <TextInput
+                  style={styles.textInput}
+                  label="Nome"
+                  value={nome}
+                  mode={"outlined"}
+                  activeOutlineColor={"#FF941A"}
+                  onChangeText={text => setNome(text)}
+                  outlineColor={"#FFF"}
+                  left={
+                     <TextInput.Icon
+                        icon="account"
+                        size={25}
+                        iconColor="#FF941A"
+                     />
+                  }
+               />
+               <TextInput
+                  style={styles.textInput}
+                  label="E-mail"
+                  value={email}
+                  mode={"outlined"}
+                  activeOutlineColor={"#FF941A"}
+                  onChangeText={text => setEmail(text)}
+                  outlineColor={"#FFF"}
+                  left={
+                     <TextInput.Icon icon="at" size={25} iconColor="#FF941A" />
+                  }
+               />
+               <TextInput
+                  style={styles.textInput}
+                  label="Senha"
+                  value={password}
+                  mode={"outlined"}
+                  activeOutlineColor={"#FF941A"}
+                  onChangeText={text => setPassword(text)}
+                  outlineColor={"#FFF"}
+                  left={
+                     <TextInput.Icon
+                        icon="lock"
+                        size={25}
+                        iconColor="#FF941A"
+                     />
+                  }
+                  secureTextEntry={showPassword}
+                  right={
+                     showPassword ? (
+                        <TextInput.Icon
+                           icon="eye"
+                           size={25}
+                           iconColor="#FF941A"
+                           onPress={() => setShowPassword(!showPassword)}
+                        />
+                     ) : (
+                        <TextInput.Icon
+                           icon="eye-off"
+                           size={25}
+                           iconColor="#FF941A"
+                           onPress={() => setShowPassword(!showPassword)}
+                        />
+                     )
+                  }
+               />
+               <Button mode="contained" style={styles.loginButton}>
+                  Cadastrar
+               </Button>
+            </View>
+         </SafeAreaView>
+      </TouchableWithoutFeedback>
    );
 };
 
-export default Register;
+const styles = StyleSheet.create({
+   container: {
+      width: "100%",
+   },
+   register: {
+      fontSize: 40,
+      fontWeight: "bold",
+      marginBottom: 0,
+   },
+   textContainer: {
+      marginBottom: 65,
+      marginTop: 175,
+      marginHorizontal: 30,
+   },
+   textInput: {
+      marginBottom: 5,
+      borderRadius: 5,
+   },
+   loginButton: {
+      paddingVertical: 4,
+      margin: 20,
+      width: "100%",
+      alignSelf: "center",
+      borderRadius: 5,
+      backgroundColor: "#FF941A",
+   },
+   createAccountText: {
+      fontWeight: "bold",
+      color: "#FF941A",
+   },
+   cardImg: {
+      width: "100%",
+      height: "60%",
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 25,
+   },
+   img: {
+      width: "100%",
+   },
+   cardInputs: {
+      marginTop: -50,
+      paddingHorizontal: 30,
+   },
+});
 
-const styles = StyleSheet.create({});
+export default Register;
