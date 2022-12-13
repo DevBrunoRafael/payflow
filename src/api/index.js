af0f93408bf5566926133f5e70433838644cb2ab
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-12-12 23:06:17 -0300

    chore: changing api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://web-production-673f.up.railway.app"
+  baseURL: "htpps://api-payflow-app-production.up.railway.app"
 });
 
 export { api };
